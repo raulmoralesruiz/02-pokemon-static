18bb449105d12a00bff0da4d157dddc1d190e427
Raúl <raulmdaw@gmail.com>
2025-07-09 19:47:02 +0200

    Fix check-favorite

--- src/components/pokemons/FavoritePokemons.tsx
+++ src/components/pokemons/FavoritePokemons.tsx
@@ -12,7 +12,7 @@ export const FavoritePokemons = () => {
   const [pokemons, setPokemons] = createSignal(getLocalStoragePokemons());
 
   return (
-    <div class="grid grid-cols-2 sm:grid-cols-4">
+    <div class="grid grid-cols-2 sm:grid-cols-4 mt-2">
       <For each={pokemons()}>
         {(pokemon) => <FavoritePokemonCard pokemon={pokemon} />}
       </For>
