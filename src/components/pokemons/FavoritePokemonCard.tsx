b1020649adbad6370800add791d47f0017c53a69
Raúl <raulmdaw@gmail.com>
2025-07-09 19:28:00 +0200

    Mostrar y remover favoritos

---
+++ src/components/pokemons/FavoritePokemonCard.tsx
@@ -0,0 +1,37 @@
+import type { FavoritePokemon } from "@interfaces/favorite-pokemon";
+import { createSignal, Show, type Component } from "solid-js";
+
+interface Props {
+  pokemon: FavoritePokemon;
+}
+
+export const FavoritePokemonCard: Component<Props> = ({ pokemon }) => {
+  const [isVisible, setIsVisible] = createSignal(true);
+  const imageSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+
+  const deleteFav = () => {
+    const favPokemons = JSON.parse(
+      localStorage.getItem("favPokemons") ?? "[]",
+    ) as FavoritePokemon[];
+    const newFavorites = favPokemons.filter((p) => p.id !== pokemon.id);
+
+    localStorage.setItem("favPokemons", JSON.stringify(newFavorites));
+    setIsVisible(false);
+  };
+
+  return (
+    <Show when={isVisible()}>
+      <div class="flex flex-col justify-center items-center">
+        <a href={`/pokemons/${pokemon.name}`}>
+          <img src={imageSrc} alt={pokemon.name} width={96} height={96} />
+          <p class="capitalize">
+            #{pokemon.id} {pokemon.name}
+          </p>
+        </a>
+        <button onClick={deleteFav} class="text-red-500">
+          Borrar
+        </button>
+      </div>
+    </Show>
+  );
+};
--- src/components/pokemons/FavoritePokemons.tsx
+++ src/components/pokemons/FavoritePokemons.tsx
@@ -1,5 +1,6 @@
 import type { FavoritePokemon } from "@interfaces/favorite-pokemon";
 import { createSignal, For } from "solid-js";
+import { FavoritePokemonCard } from "./FavoritePokemonCard";
 
 const getLocalStoragePokemons = (): FavoritePokemon[] => {
   const favPokemons = JSON.parse(localStorage.getItem("favPokemons") ?? "[]");
@@ -12,7 +13,9 @@ export const FavoritePokemons = () => {
 
   return (
     <div class="grid grid-cols-2 sm:grid-cols-4">
-      <For each={pokemons()}>{(pokemon) => <h1>{pokemon.name}</h1>}</For>
+      <For each={pokemons()}>
+        {(pokemon) => <FavoritePokemonCard pokemon={pokemon} />}
+      </For>
     </div>
   );
 };
